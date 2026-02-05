ac6449b6f431dbe6b24a3f8b5bf10cd7da353051
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2026-02-05 22:44:05 +0530

    fix: Remove unused cosine similarity function from utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -50,24 +50,3 @@ export function mergeRetrievalResults<T extends { chunkId: string; score: number
     .sort((a, b) => b.score - a.score)
     .slice(0, maxResults);
 }
-
-/**
- * Calculate cosine similarity between two vectors.
- */
-export function cosineSimilarity(a: number[], b: number[]): number {
-  if (a.length !== b.length) {
-    throw new Error('Vectors must have same dimensions');
-  }
-
-  let dotProduct = 0;
-  let normA = 0;
-  let normB = 0;
-
-  for (let i = 0; i < a.length; i++) {
-    dotProduct += a[i] * b[i];
-    normA += a[i] * a[i];
-    normB += b[i] * b[i];
-  }
-
-  return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
-}
